Fixed small error in X_dot computation, I was copying the 1st-index X_dot to the 0th-index because I didn't know it would make a big difference, but it inflated the tangling values, especially in the CX df. So I modified so it grabs the last index within the respective cycle condition and subtracts it, since the cycles are periodic. If the NeurIPS paper gets accepted, I should go back and regenerate all the figures, despite it making a barely noticeable difference in the color scheme.

neuro_rl/analysis/analyze_tangling.py
@@ -35,7 +35,14 @@ def compute_tangling(X: np.array, t: np.array):
     # compute derivatives of X
     X_dot = np.diff(X, axis=0) / dt
     X_dot = np.insert(X_dot, 0, X_dot[0,:], axis=0)
-    X_dot[np.where(t_diff < 0)[0],:] = X_dot[np.where(t_diff < 0)[0] + 1,:]
+
+    # find first time step
+    first_indices = np.where(t == 0)[0]
+    last_indices = np.roll(first_indices, -1) - 1
+    last_indices[-1] = len(t) - 1
+
+    for i in range(len(first_indices)):
+        X_dot[first_indices[i], :] = ( X[first_indices[i], :] - X[last_indices[i], :] ) / dt  # X_dot = X[first time step w/in CONDITION] - X[last time step w/in CONDITION] 
  
     # compute constant, prevents denominator from shrinking to zero
     epsilon = 0.1 * np.var(X)